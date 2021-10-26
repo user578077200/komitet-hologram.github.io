9fdf5ca00d68e4acf76b0860f33acefc0cea459e
Stepan Snegirev <stepan@handmind.ru>
2021-10-26 11:11:39 +0300

    ÷

--- build/tasks/browsersync.js
+++
@@ -1,9 +0,0 @@
-const browserSync = require('browser-sync');
-
-const config = require('../config').browsersync;
-
-module.exports = function (cb) {
-  return browserSync.create().init(config, cb);
-};
-
-
--- build/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-const del = require('del');
-
-const config = require('../config').clean;
-
-module.exports = function (cb) {
-  del(config, { dot: true }).then(() => cb(null));
-};
-
-
